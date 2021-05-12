bf9f4c90cdcbb97bda1e0f570ceb9c718485cc5a
Cristiano Rausse <cristiano.rausse@gmail.com>
2021-05-12 16:51:22 -0300

    d_pyramid.c

--- d_pyramid.c
+++ d_pyramid.c
@@ -34,15 +34,15 @@ int size(void)
 /*Function to build the pyramid*/
 void build_pyramid(int Order_of_Pyramid)
 {
-    int New = (Order_of_Pyramid * 2) + 2; //Novo tamanho das colunas
-    int counter = 0; // decremento para cada linha da pirâmide
-    int Counter = Order_of_Pyramid + 2; //inicio da contagem na 2a pirâmide
+    int New = (Order_of_Pyramid * 2) + 2; //New size for the columns
+    int counter = 0; //Decrement of each line of the pyramid
+    int Counter = Order_of_Pyramid + 2; //Start of the count for the second pyramid
 
-    for (int i = 0; i < Order_of_Pyramid; i++) // For para cada linha
+    for (int i = 0; i < Order_of_Pyramid; i++) // For referring to each line
     {
         counter++;
         Counter++;
-        for (int j = 0; j < New; j++)// For para cada coluna -> New variable N to represent the extended size of matrix (offset => 2 because of the gaps between the pyramids)
+        for (int j = 0; j < New; j++)// For referring to each column -> New variable N to represent the extended size of matrix (offset => 2 because of the gaps between the pyramids)
         {
                 if (j < Order_of_Pyramid - counter)
                 {
@@ -55,11 +55,11 @@ void build_pyramid(int Order_of_Pyramid)
 
             while(j == Order_of_Pyramid || (j > Order_of_Pyramid && j < New))// This loop is for the first matrix (n represents the order of the matrix)
             {
-                if (j >= Counter) //parte para os gaps
+                if (j >= Counter) //part for the gaps
                 {
                     printf(" ");
                 }
-                else if (j == Order_of_Pyramid  || j == Order_of_Pyramid + 1)// parte para o offset de 2
+                else if (j == Order_of_Pyramid  || j == Order_of_Pyramid + 1)// part for the offset of 2
                 {
                     printf(" ");
                 }
